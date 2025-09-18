985484a5bb2f18b0369a5abbd150dfceb42e7da5
AshanOdi <ashanuni25@gmail.com>
2025-09-18 22:11:22 +0530

    ENtites created

---
+++ src/main/java/com/example/entgra/EntgraApplication.java
@@ -0,0 +1,13 @@
+package com.example.entgra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntgraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntgraApplication.class, args);
+	}
+
+}
